f5c70075a0ebcc045a3a536313c6b77bf0ac9328
Christian Wang <christianwang39@gmail.com>
2025-07-29 19:03:59 +0200

    connected to kubiks

---
+++ instrumentation.ts
@@ -0,0 +1,9 @@
+export async function register() {
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
+    const { KubiksSDK } = await import('@kubiks/otel-nextjs');
+    const sdk = new KubiksSDK({
+      service: 'huginn', // Replace with your service name
+    });
+    sdk.start();
+  }
+} 
