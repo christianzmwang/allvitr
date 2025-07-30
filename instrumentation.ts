d45d4899a051e30782fd501b4414df88edb1c352
christianzmwang <christianwang39@gmail.com>
2025-07-30 23:14:01 +0200

    corrected instrumetation.ts

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,12 +1,12 @@
+// instrumentation.ts
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
-    const { KubiksSDK } = await import('@kubiks/otel-nextjs');
+    const { KubiksSDK } = await import('@kubiks/otel-nextjs')
+
     const sdk = new KubiksSDK({
       service: 'hugin',
-      endpoint: process.env.KUBIKS_ENDPOINT || 'https://api.kubiks.com',
-      apiKey: process.env.KUBIKS_API_KEY,
-      environment: process.env.NODE_ENV || 'development',
-    });
-    sdk.start();
+    })
+
+    sdk.start()
   }
-} 
+}
