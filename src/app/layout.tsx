5778a2202c81c7c80171c45c30f9d1611c1c134a
Christian Wang <christianwang39@gmail.com>
2025-07-31 15:54:58 +0200

    vercel analytics setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -26,6 +28,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
+	<SpeedInsights />
       </body>
     </html>
   );
