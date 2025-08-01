06b7de8ccbab8d69334d0c08026e03368df2c03f
christianzmwang <christianwang39@gmail.com>
2025-08-02 00:02:12 +0200

    removed kubiks ai

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 export default nextConfig;
-=======
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {
-  /* config options here */
-}
-
-export default nextConfig
->>>>>>> 32b5adf92c7a4560b39c6726bfae205608549105
