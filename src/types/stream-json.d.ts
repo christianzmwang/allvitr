9764644d98897224faefe634c877c262de1853e8
Christian Wang <christianwang39@gmail.com>
2025-08-14 21:21:04 +0200

    fixed database integration failure

--- src/types/stream-json.d.ts
+++ src/types/stream-json.d.ts
@@ -1,9 +1,9 @@
 declare module 'stream-json/Parser' {
-  export function parser(options?: any): any;
+  export function parser(options?: Record<string, unknown>): NodeJS.ReadableStream;
 }
 
 declare module 'stream-json/streamers/StreamArray' {
-  export function streamArray(): any;
+  export function streamArray(): NodeJS.ReadableStream;
 }
 
 
